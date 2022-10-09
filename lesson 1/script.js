38eb9d6ec8a0965b5ace354ae64359185372c91f
nookismile <stina.novickova@yandex.ru>
2022-10-09 19:26:34 +0300

    lesson 1

---
+++ lesson 1/script.js
@@ -0,0 +1,61 @@
+// task 1
+//
+// let firstValue = 17;
+// let secondValue = 19;
+// let thirdValue = 25;
+//
+// let firstValueSant = firstValue * 2.54;
+// let secondValueSant = secondValue * 2.54;
+// let thirdValueSant = thirdValue * 2.54;
+//
+// console.log(firstValueSant);
+// console.log(secondValueSant);
+// console.log(thirdValueSant);
+
+
+// task 2
+
+// function division() {
+//     let number = 123456789;
+//
+//     if (number % 3 === 0) {
+//         console.log('true');
+//         return true;
+//     }
+//
+//     console.log('false');
+//     return false;
+// }
+//
+// division();
+
+
+// task 3
+//
+//let sum2 = 500 + 600 + 700;
+// console.log(sum);
+
+
+// task 4
+//
+//let count = 3;
+// console.log(counts);
+
+
+// task 5
+// const AVERAGE = sum2 / count;
+// console.log(`Среднее арифметическое: ${AVERAGE}`);
+
+
+// task 6
+// sum = 3;
+// count2 = 13;
+// specialValue = 14;
+
+
+// task 7
+//
+// console.log(Math.sqrt(2));
+// console.log(Math.sqrt(3));
+// console.log(Math.sqrt(4));
+// console.log(Math.sqrt(7));
